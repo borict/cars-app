77950458f5c23f632d48bbdba79279f7a7c0e757
borict <tamara.boric8@gmail.com>
2023-01-04 15:00:12 +0100

    Zadatak 2

--- src/pages/AppCars.jsx
+++ src/pages/AppCars.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { carService } from "../services/CarService";
+import { useState, useEffect } from "react";
+import { CarDetail } from "../components/CarDetail.component";
 
 export const AppCars = () => {
-  return  (
-    <div>
-        <p>cao</p>
-    </div>
-  );
+  const [carList, setCarList] = useState([]);
+
+
+  const getHandler = async () => {
+    const response = await carService.getAll();
+    setCarList(response.data);
+  };
+ 
+  useEffect(() => {
+    getHandler();
+  }, []);
+
+  return  (<div>
+  <ul>
+    {carList.map((car) => (
+      <li key={car.id}>
+        <CarDetail
+          id={car.id} 
+          brand={car.brand}
+          model={car.model}
+          year={car.year}
+          maxSpeed={car.maxSpeed}
+          isAutomatic={car.isAutomatic}
+          engine={car.engine}
+          numberOfDoors={car.numberOfDoors}
+        />
+      </li>
+    ))}
+  </ul>
+</div>
+  )
 
 };
---
+++ src/services/ApiService.js
@@ -0,0 +1,8 @@
+import { httpService } from "./HttpService";
+
+export default class ApiService {
+    constructor() {
+        this.client = httpService.httpClient;
+
+    }
+}
